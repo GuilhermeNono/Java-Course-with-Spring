7314cb88e85822ef36e6848c2f34d20ea7d8d7fe
Guilherme Nono <guilherme.nono45789@gmail.com>
2023-01-20 08:48:35 -0300

    First Commit

---
+++ src/main/java/me/nono/firststeps/FirstStepsApplication.java
@@ -0,0 +1,13 @@
+package me.nono.firststeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstStepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nono/firststeps/FirstStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nono.firststeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
